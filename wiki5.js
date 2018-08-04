b1966e7c6b7cc3bbd54a902243956916527ea079
John Desrosiers <johndrunner@yahoo.com>
2018-08-04 16:35:05 -0700

    Update wiki5.js
    Parse the first 5 events and births.

--- wiki5.js
+++ wiki5.js
@@ -1,5 +1,24 @@
 // --the code below works and renders, but returns EVERYTHING 
 // --now, trying to experiment with the Object method. Tried to do action=parse, which helped to grab the 'sections' to better target the items in the 'extract' object
+function findListData(html, id) {
+    const $document = $(html);
+    const list = $document
+        .find(`#${id}`)
+        .parent()
+        .next()
+        .children()
+        .toArray()
+        .slice(0, 5)
+        .map(el => $(el).text());
+    return list;
+}
+
+function parseWikiHtml(html) {
+    const $document = $(html);
+    const events = findListData(html, 'Events');
+    const births = findListData(html, 'Births');
+    return { events, births };
+}
 
 $(document).ready(function () {
 
@@ -18,9 +37,12 @@ $(document).ready(function () {
             dataType: "json",
             success: function (data) {
                 console.log(url);
-                console.log(data);
-                console.log(data.query.pages);
-                console.log(Object.values(data.query.pages)[0].extract);
+                // console.log(data);
+                // console.log(data.query.pages);
+                // console.log(Object.values(data.query.pages)[0].extract);
+                const firstPageHtml = Object.values(data.query.pages)[0].extract;
+                const wikiDateData = parseWikiHtml(firstPageHtml);
+                console.log('wikiDateData:', wikiDateData);
 
                 $('#displayWiki').html('');
 
